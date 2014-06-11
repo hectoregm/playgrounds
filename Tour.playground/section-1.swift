9e007392fd238ec0518e969c203f63fafd6d146e
Hector E. Gomez Morales <hectoregm@gmail.com>
2014-06-11 16:58:51 -0500

    Added playground for examples in Swift Tour page.

---
+++ Tour.playground/section-1.swift
@@ -0,0 +1,19 @@
+println("Hello, world")
+
+let four: Float = 4
+
+let label = "The width is "
+let width = 94
+let widthLabel = label + String(width)
+
+let apples = 3
+let oranges = 5
+let appleSummary = "I have \(apples) apples."
+let fruitSummary = "I have \(apples + oranges) pieces of fruit."
+
+let name = "Hector"
+let price = 10.0
+
+let message = "Hi \(name), the price in dollars is \(price) and in pesos \(price * 13.0)"
+
+var shoppingList = ["catfish", "water", "tulips", "blue paint"]
